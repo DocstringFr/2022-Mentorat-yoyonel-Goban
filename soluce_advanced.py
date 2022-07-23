52ae49e4583ef8b821a19df99ba9016b027e917b
ATTY Lionel <latty@unowhy.com>
2022-07-23 15:51:00 +0200

    refactor(is_taken): use an other enum type for walk map (to increase readibility)
    Signed-off-by: ATTY Lionel <latty@unowhy.com>

--- soluce_advanced.py
+++ soluce_advanced.py
@@ -1,3 +1,4 @@
+import enum
 from dataclasses import dataclass, astuple
 from typing import List
 
@@ -20,6 +21,15 @@ class Position:
         return Position(self.x + other.x, self.y + other.y)
 
 
+class StatusWalkMap(enum.Enum):
+    """
+    Enum representing the Status of a position on a walk map
+    """
+
+    NOT_VISITED = False
+    VISITED = True
+
+
 class SolutionAdvancedGoban(OpenClassRoom_Goban):
     # https://docs.python.org/3/library/typing.html#type-aliases
     # https://docs.python.org/3/library/typing.html#typing.Final
@@ -35,22 +45,23 @@ class SolutionAdvancedGoban(OpenClassRoom_Goban):
         super().__init__(goban)
         self._board_height = len(self.goban)
         self._board_width = len(self.goban[0])
-        self._walk_map: List[List[Status]] = []
+        self._walk_map: List[List[StatusWalkMap]] = []
 
     def _init_walk_map(self):
         self._walk_map = [
-            [Status.EMPTY] * self._board_width for _ in range(self._board_height)
+            [StatusWalkMap.NOT_VISITED] * self._board_width
+            for _ in range(self._board_height)
         ]
 
-    def _get_status_on_walk_map(self, position: Position) -> Status:
+    def _get_status_on_walk_map(self, position: Position) -> StatusWalkMap:
         return self._walk_map[position.y][position.x]
 
-    def _set_status_on_walk_map(self, position: Position, status: Status):
+    def _set_status_on_walk_map(self, position: Position, status: StatusWalkMap):
         self._walk_map[position.y][position.x] = status
 
     def _recursive_is_taken(self, position: Position, current_status: Status) -> bool:
         # update the walk-map (can be accessed because on Goban::is_taken scope)
-        self._set_status_on_walk_map(position, current_status)
+        self._set_status_on_walk_map(position, StatusWalkMap.VISITED)
 
         # retrieve all neighbors status
         neighbors_status = [
@@ -71,13 +82,17 @@ class SolutionAdvancedGoban(OpenClassRoom_Goban):
         for dir_neighbor, status_neighbor in zip(
             SolutionAdvancedGoban.DIR_NEIGHBORS, neighbors_status
         ):
+            # [filter] continue only if neighbor status is not EMPTY or OUT,
+            #          or the neighbor stone is not the same color of the current stone
+            if (
+                status_neighbor in (Status.EMPTY, Status.OUT)
+                or status_neighbor != current_status
+            ):
+                continue
             # neighbor position
             next_position = position + dir_neighbor
             # [search] if any neighbors give at least one free mouvement
-            if (
-                status_neighbor == current_status
-                and self._get_status_on_walk_map(next_position) != current_status
-            ):
+            if self._get_status_on_walk_map(next_position) != StatusWalkMap.VISITED:
                 # [rec] recursive call on shape's neighbor
                 result_is_taken &= self._recursive_is_taken(
                     next_position, current_status
